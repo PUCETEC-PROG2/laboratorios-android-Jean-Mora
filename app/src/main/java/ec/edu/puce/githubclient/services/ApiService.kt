5be3e184cebcd5c1d310ce5b67d15b9b5c6d088c
Jean-Mora <mora5252@hotmail.com>
2026-05-21 14:34:36 -0500

    Laboratorio 2 finalizado

--- app/src/main/java/ec/edu/puce/githubclient/services/ApiService.kt
+++ app/src/main/java/ec/edu/puce/githubclient/services/ApiService.kt
@@ -3,11 +3,17 @@ package ec.edu.puce.githubclient.services
 import ec.edu.puce.githubclient.models.Repository
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
 
     @GET("users/{username}/repos")
     suspend fun getRepositories(
-        @Path("username") username: String
+        @Path("username") username: String,
+        @Query(value = "sort") created : String = "created",
+        @Query(value = "direction") direction : String = "desc",
+        @Query(value = "affiliation") affiliation : String = "owner",
+        @Query(value = "per_page") perPage: Int = 100,
+        @Query(value = "t") t: String = "${System.currentTimeMillis()}",
     ): List<Repository>
 }
